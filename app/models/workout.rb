ea567cdb272864b7ad80de8d28b9dd684229d33f
Justin Rohrman <rohrmanj@gmail.com>
2024-02-04 14:14:09 -0600

    notes on workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,9 @@
 class Workout < ApplicationRecord
+  # Abstract things a workout needs
+  #   begin date / time
+  #   end date / time
+  #   activities
+  #     examples: resistance training, bjj, cardio
+  #   notes
+  #   location?
 end
