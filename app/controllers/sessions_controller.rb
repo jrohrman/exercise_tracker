783ae353dd98547fe54929e1eb909c4df3827bab
Justin Rohrman <rohrmanj@gmail.com>
2025-03-14 18:37:08 -0500

    failed login flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Successfully logged in!'
     else
-      flash.now[:alert] = 'Invalid email/password combination'
-      render :new
+      flash[:alert] = 'Invalid email/password combination'
+      redirect_to login_path, status: :unprocessable_entity
     end
   end
 
